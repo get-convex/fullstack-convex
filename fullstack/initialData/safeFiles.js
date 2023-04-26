39a57cf31ec88de1bccb1cf7204c5cffc2f73a12
Anjana Vakil <anjanavakil@gmail.com>
2023-04-25 21:37:33 -0400

    Reformat & prune initialData files (closes #27)

---
+++ fullstack/initialData/safeFiles.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    creationTime: 1680231018327.0483,
+    id: 'DegnNr0WHzeRjT3PlDZL9w',
+    name: 'colors.png',
+    sha256: '78a0ba4fce51b170fa0328365161c34f6e6f6224711383bd5678e00250344d0f',
+    storageId: '0d4547d2-95bc-4bb2-acee-ed591ddfc2e7',
+  },
+  {
+    creationTime: 1680231093832.7651,
+    id: 'PdUa70Pja1o-1Di83-FL9w',
+    name: 'swirls.png',
+    sha256: '214796089ad60dab106a14f09fc2e6e17cd0153b4ecf5c2da3c83b87dd56f05d',
+    storageId: '86d68600-c4e6-4d2e-a97c-7cbfc6290872',
+  },
+  {
+    creationTime: 1680231043034.1724,
+    id: 'hAcAjGQuZ_pg7tMWCthL9w',
+    name: 'leaves.png',
+    sha256: 'c0c0499b38d0af94cb94f9fa65b34b9b490c0ce8656b4214a6f2b37f1a17cddf',
+    storageId: '08b2dba1-4064-4d3d-a44a-780c8c9651c2',
+  },
+  {
+    creationTime: 1680231065862.407,
+    id: 'taGRrRsU9J0X9JVKM-lL9w',
+    name: 'shapes.png',
+    sha256: 'd4fe004723895a71ef5fac73777470d546af36999f7bc436b088bea171c9c93d',
+    storageId: '6d26bc65-02f6-4926-8160-def10c7187d7',
+  },
+  {
+    creationTime: 1680224902717.7102,
+    id: 'xWqIYQbmHM0EzyouGJBL9w',
+    name: 'abstract.png',
+    sha256: '76407c59f4d6ded7e39ac9fdbbcc8e2e1d5c6dfc9392dda43476a50afebc7015',
+    storageId: '22b3af54-fd3f-4ebd-baab-462e860b782a',
+  },
+]

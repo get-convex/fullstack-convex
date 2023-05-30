00d275bafd9f51aa822b46736b46ede87c07c4cb
Anjana Vakil <anjanavakil@gmail.com>
2023-05-30 15:15:21 +0200

    Fix useStablePaginatedQuery

--- convex-hooks/useStableQuery.ts
+++ convex-hooks/useStableQuery.ts
@@ -23,7 +23,7 @@ export const useStablePaginatedQuery = ((name, args, options) => {
 
   // If data is still loading, wait and do nothing
   // If data has finished loading, store the results array
-  if (isLoading) {
+  if (!isLoading) {
     stored.current = result.results
   }
 

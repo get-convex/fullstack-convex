8c65eb3744a2b05c2d842c87a96dc35efd083abf
Anjana Vakil <anjanavakil@gmail.com>
2023-06-07 14:44:06 +0200

    Update convex to 0.16.0-alpha.0

---
+++ convex/auth.config.js
@@ -0,0 +1,12 @@
+export default {
+  providers: [
+    {
+      "applicationID": "639AOoxTqneICxl2kQtb0sYpturbT1MP",
+      "domain": "https://dev-q6pbtli453usw7ul.us.auth0.com/"
+    },
+    {
+      "applicationID": "S6FtqbevxZetjN5SiREChsKeXNQb28x7",
+      "domain": "https://fullstack-convex-staging.us.auth0.com/"
+    }
+  ],
+};
